7718711767d8fc59de45288a210d6d4062135a35
Octavio Elias Lima <62713565+octaviolima@users.noreply.github.com>
2022-02-22 11:53:33 -0800

    Text Data Visualization

--- project/nlp_ml_proj.R
+++ project/nlp_ml_proj.R
@@ -6,14 +6,12 @@ library( pacman )
 p_load( fastverse, magrittr, here, skimr, dplyr, ggplot2, ggthemes, equatiomatic,
         gridExtra, caret, naivebayes, knitr, kableExtra, shiny, data.table, 
         tidymodels, ggthemes, wordcloud, tm, SnowballC, RColorBrewer, e1071,
-        data.table
+        data.table, stringr
         )
-
 
 ##############################################################################
 # Load Data
 ##############################################################################
-
 
 # Loading email dataset: consists of "text data" and "dummy variable"
 email_df <- data.table::fread( "spam_or_not_spam.csv", header = T )
@@ -21,11 +19,9 @@ email_df <- data.table::fread( "spam_or_not_spam.csv", header = T )
 # Factoring dummy variable
 email_df$label <- factor( email_df$label )
 
-
 ##############################################################################
 # Stem Document Tranformation
 ##############################################################################
-
 
 # To retrieve the root of a word (eg, doing -> do), options are "stemming" &
 # "lemmatization".
@@ -48,11 +44,25 @@ email_corpus <- VCorpus(
 email_corpus <- tm::tm_map( email_corpus, 
                             tm::stemDocument )
 
+# Removing stopwords
+email_corpus = tm_map( email_corpus, 
+                       removeWords, stopwords( "en" ) )
+
 # DocumentTermMatrix(): tokenize the email corpus.
-sms_dtm <- tm::DocumentTermMatrix(email_corpus)
+email_dtm <- tm::DocumentTermMatrix(email_corpus)
 
+# 
+reverse_email <- data.frame(
+                text = sapply( email_corpus, as.character ), 
+                stringsAsFactors = FALSE, 
+                type = email_df$label
+                )
 
-
+# Visualing text data after cleaning and pre-processing
+wordcloud( reverse_email$text, 
+           max.words = 150, 
+           colors = brewer.pal( 7, "Dark2" ), 
+           random.order = FALSE ) 
 
 
 
