591dc0ac5fd4a84e85f5798591f3cab20ca3d797
Octavio Elias Lima <62713565+octaviolima@users.noreply.github.com>
2022-02-22 12:18:29 -0800

    splits

--- project/nlp_ml_proj.R
+++ project/nlp_ml_proj.R
@@ -2,6 +2,8 @@
 # Load packages
 ##############################################################################
 
+
+
 library( pacman )
 p_load( fastverse, magrittr, here, skimr, dplyr, ggplot2, ggthemes, equatiomatic,
         gridExtra, caret, naivebayes, knitr, kableExtra, shiny, data.table, 
@@ -9,9 +11,13 @@ p_load( fastverse, magrittr, here, skimr, dplyr, ggplot2, ggthemes, equatiomatic
         data.table, stringr
         )
 
+
+
 ##############################################################################
 # Load Data
 ##############################################################################
+
+
 
 # Loading email dataset: consists of "text data" and "dummy variable"
 email_df <- data.table::fread( "spam_or_not_spam.csv", header = T )
@@ -19,18 +25,21 @@ email_df <- data.table::fread( "spam_or_not_spam.csv", header = T )
 # Factoring dummy variable
 email_df$label <- factor( email_df$label )
 
+
+
 ##############################################################################
-# Stem Document Tranformation
+# Pre Processing & Cleaning (stemming)
 ##############################################################################
+
+
 
 # To retrieve the root of a word (eg, doing -> do), options are "stemming" &
 # "lemmatization".
 
 # STEMMING:      faster but maybe not as effective
 # LEMMATIZATION: slower but more effective
-# More on this:  https://towardsdatascience.com/stemming-vs-lemmatization-2daddabcb221
+# More on this:  towardsdatascience.com/stemming-vs-lemmatization-2daddabcb221
 
-##############################################################################
 
 # VectorSource(): creates one document for each email 
 # Vcorpus():      creates a volatile corpus from these individual emails
@@ -45,13 +54,29 @@ email_corpus <- tm::tm_map( email_corpus,
                             tm::stemDocument )
 
 # Removing stopwords
-email_corpus = tm_map( email_corpus, 
+email_corpus <- tm_map( email_corpus, 
                        removeWords, stopwords( "en" ) )
+
+# OPTIONAL -- Removing two most frequent stopwords: "NUMBER", "URL"
+# email_corpus <- tm_map( email_corpus, 
+#                        removeWords, c("NUMBER", "url", "urI", "URL") )
+
+# OPTIONAL -- Removing extra white space
+# email_corpus <- tm_map( email_corpus,
+#                         stringr::str_squish)
 
 # DocumentTermMatrix(): tokenize the email corpus.
 email_dtm <- tm::DocumentTermMatrix(email_corpus)
 
-# 
+
+
+##############################################################################
+# Visualizing cleaned data
+##############################################################################
+
+
+
+# new data.frame with cleaned data
 reverse_email <- data.frame(
                 text = sapply( email_corpus, as.character ), 
                 stringsAsFactors = FALSE, 
@@ -63,6 +88,88 @@ wordcloud( reverse_email$text,
            max.words = 150, 
            colors = brewer.pal( 7, "Dark2" ), 
            random.order = FALSE ) 
+
+
+
+
+##############################################################################
+# Subsetting SPAM vs. NON-SPAM data; checking most frequent words there
+##############################################################################
+
+ 
+
+# Subsetting to spam == 1
+spam <- reverse_email %>% filter( type == 1 )
+
+# Visualing spam data
+wordcloud( spam$text, 
+           max.words = 150, 
+           colors = brewer.pal( 7, "Dark2" ), 
+           random.order = FALSE ) 
+
+
+
+# Subsetting to spam == 1
+ham <- reverse_email %>% filter( type == 0 )
+
+# Visualing spam data
+wordcloud( ham$text, 
+           max.words = 150, 
+           colors = brewer.pal( 7, "Dark2" ), 
+           random.order = FALSE ) 
+
+
+
+
+##############################################################################
+# SPLITS
+##############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
